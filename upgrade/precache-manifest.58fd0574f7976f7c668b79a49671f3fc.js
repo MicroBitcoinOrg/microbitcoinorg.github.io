9d22113112a0d1e8a4cef709be539d770934f725
charkala <charkala@protonmail.com>
2019-03-05 23:25:07 +0900

    Updated build files.

---
+++ upgrade/precache-manifest.58fd0574f7976f7c668b79a49671f3fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ca0d90f3b4e63d33994b435e1e9ab812",
+    "url": "/upgrade/static/media/forest.ca0d90f3.mp4"
+  },
+  {
+    "revision": "471f517e3471f1222bb2",
+    "url": "/upgrade/static/js/runtime~main.471f517e.js"
+  },
+  {
+    "revision": "59df296608fb175ce489",
+    "url": "/upgrade/static/js/main.59df2966.chunk.js"
+  },
+  {
+    "revision": "34f7e0e627e5500424fc",
+    "url": "/upgrade/static/js/2.34f7e0e6.chunk.js"
+  },
+  {
+    "revision": "59df296608fb175ce489",
+    "url": "/upgrade/static/css/main.a04257a8.chunk.css"
+  },
+  {
+    "revision": "06b233acf4ad6cb5d011d23fcf5aa7c5",
+    "url": "/upgrade/index.html"
+  }
+];
